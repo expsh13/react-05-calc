f372b2cc21600659fe7e5774bfa877e2369d544c
Satoki Hata <satokihata@gmail.com>
2024-02-12 00:01:36 +0900

    update css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,49 @@
+import { ReactNode } from "react";
 import { Button } from "./components/Button/Button";
 import "./output.css";
 
+type ButtonLineType = {
+  children: ReactNode;
+};
+
+const ButtonLine = (props: ButtonLineType) => {
+  const { children } = props;
+  return <div className="flex gap-1">{children}</div>;
+};
+
 export const App = () => {
-  return <Button text="a" />;
+  return (
+    <div className="w-80 mx-auto">
+      <p className="bg-white h-8 border border-gray-800 rounded mb-4"></p>
+      <div className="grid gap-1">
+        <ButtonLine>
+          <Button text="1" />
+          <Button text="2" />
+          <Button text="3" />
+          <Button text="+" />
+        </ButtonLine>
+        <ButtonLine>
+          <Button text="4" />
+          <Button text="5" />
+          <Button text="6" />
+          <Button text="-" />
+        </ButtonLine>
+        <ButtonLine>
+          <Button text="7" />
+          <Button text="8" />
+          <Button text="9" />
+          <Button text="*" />
+        </ButtonLine>
+        <ButtonLine>
+          <Button text="c" />
+          <Button text="0" />
+          <Button text="." />
+          <Button text="/" />
+        </ButtonLine>
+        <ButtonLine>
+          <Button text="=" />
+        </ButtonLine>
+      </div>
+    </div>
+  );
 };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,5 +4,9 @@ type PropsType = {
 
 export const Button = (props: PropsType) => {
   const { text } = props;
-  return <p className="text-red-900">{text}</p>;
+  return (
+    <button className="bg-gray-500 text-gray-100 hover:bg-blue-500 rounded w-8 h-8">
+      {text}
+    </button>
+  );
 };
